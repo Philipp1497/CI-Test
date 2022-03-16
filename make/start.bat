88f2497c743bb4d8cc383c7bebc3bbe534e0f23a
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-03-16 17:59:24 +0100

    Makefile-Test

--- make/start.bat
+++ make/start.bat
@@ -2,7 +2,6 @@ cd make
 set "dir=%cd%"
 echo %PATH%
 C:\MinGW\msys\1.0\bin\make.exe ^
-	--directory=%dir% ^
 	--makefile=/c/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/Test/Pipeline1/make/makefile
 
 pause
