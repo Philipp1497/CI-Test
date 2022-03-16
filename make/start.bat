cdaa0bb788f12207cacaeb96b84722cfabefbeae
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-03-16 14:25:04 +0100

    Makefile-Test

--- make/start.bat
+++ make/start.bat
@@ -1,9 +1,5 @@
-rem cd C:\Program Files (x86)\GnuWin32\bin
+cd make
 C:\MinGW\msys\1.0\bin\make.exe
 copy *.o ..\results 2>&1
 del *.o 2>&1
 pause
-
-
-rem C:\Program Files(x86)\GnuWin32\bin\make.exe
-rem C:\Users\Philipp\Desktop\TestOrdner
