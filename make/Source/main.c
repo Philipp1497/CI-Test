e591be762266aeb8bb54bff793c4248ebb43155f
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-03-18 12:40:55 +0100

    Makefile-Test

---
+++ make/Source/main.c
@@ -0,0 +1,12 @@
+#include "include.h"
+#include <stdio.h>
+void main()
+{
+        int zahl1 = 2;
+        int zahl2 = 5;
+        int ergebnis;
+        ergebnis = addiere(zahl1,zahl2);
+        printf("Das Ergebnis ist: ");
+        printf("%d",ergebnis);
+        printf("\n");
+}
