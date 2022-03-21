7680637f406728acbf55a8a417ccbe04ffda66a3
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-03-21 10:41:07 +0100

    Makefile-Test

--- Tools/make/start.bat
+++ Tools/make/start.bat
@@ -1,7 +1,6 @@
 cd make
 set "dir=%cd%"
-rem for /f %%a in ('C:\msys64\usr\bin\cygpath.exe %dir%') do set SOURCEPATH=%%a
-set "SOURCEPATH=../../Source"
+set "SOURCEPATH=..\..\Source"
 echo %PATH%
 %MAKE_PATH%
 pause
